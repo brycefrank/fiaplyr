af1522b3914c1da26d44989dbda8f3a97217945c
brycefrank <bfrank70@gmail.com>
2026-06-07 12:30:08 -0700

    adding action for docs website

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://brycefrank.com',
+	base: '/fiaplyr/',
 	integrations: [
 		starlight({
 			title: 'fiaplyr',
